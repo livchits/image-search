fbb310e749846578f567cc0b6ea5c612827edfe9
Lucas Livchits <livchitsl@gmail.com>
2020-12-30 17:53:03 -0300

    Initial setup

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,23 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-postcss',
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: 'eslint --fix "src/**/*.{js,jsx,ts,tsx}"',
+        // Optional: Use npm package "watch" to run on every file change
+        watch: 'watch "$1" src',
+      },
+    ],
+    ['@snowpack/plugin-webpack'],
+  ],
   install: [
     /* ... */
   ],
