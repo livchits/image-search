f591c89c4c76053a0ca87989bc5101f63d84f6d2
Lucas Livchits <livchitsl@gmail.com>
2020-12-31 01:54:28 -0300

    Add access key to headers

--- src/api/getImages.js
+++ src/api/getImages.js
@@ -1,9 +1,13 @@
 const URL = import.meta.env.SNOWPACK_PUBLIC_API_URL;
 const ACCESS_KEY = import.meta.env.SNOWPACK_PUBLIC_API_ACCESS_KEY;
-const ENDPOINT = `${URL}client_id=${ACCESS_KEY}&page=1&query=`;
 
 async function getImages(query) {
-  const response = await fetch(`${ENDPOINT}${query}`);
+  const response = await fetch(`${URL}${query}`, {
+    headers: {
+      Authorization: `Client-ID ${ACCESS_KEY}`,
+      'Accept-Version': 'v1',
+    },
+  });
   const { results } = await response.json();
   return results;
 }
