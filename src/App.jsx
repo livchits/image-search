6d14527b2e7078c34cb22cfdd81679b54b1c624b
Lucas Livchits <livchitsl@gmail.com>
2021-01-25 19:49:15 -0300

    Add a Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import * as React from 'react';
 import useDataImages from './useDataImages';
 import SearchForm from './components/SearchForm';
 import SearchResult from './components/SearchResult';
+import Footer from './Footer';
 
 function App() {
   const [query, setQuery] = React.useState(null);
   const [dataImages, error] = useDataImages(query);
 
   return (
-    <main className="min-h-screen bg-gradient-to-t from-indigo-200 to-indigo-50">
-      <SearchForm setQuery={setQuery} />
-      <SearchResult dataImages={dataImages} error={error} />
-    </main>
+    <>
+      <main className="flex-grow">
+        <SearchForm setQuery={setQuery} />
+        <SearchResult dataImages={dataImages} error={error} />
+      </main>
+      <Footer />
+    </>
   );
 }
 
