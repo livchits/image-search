cf8059e9d441375e7ad4fd8412e2153a34088ba5
Lucas Livchits <livchitsl@gmail.com>
2021-01-26 20:14:43 -0300

    Add link to my portfolio

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,14 +3,14 @@ import * as React from 'react';
 function Footer() {
   return (
     <footer className="w-5/6 max-w-screen-sm pt-2 pb-2 m-6 mx-auto font-mono text-base leading-relaxed text-center text-gray-800 bg-white rounded-md bg-opacity-20 sm:flex sm:justify-center">
-      <p className="sm:mr-4">
+      <a className="sm:mr-4" href="https://lucasdev.me">
         Made by Lucas Livchits{' '}
         <span aria-label="The emoji of a rocket flying" role="img">
           🚀
         </span>
-      </p>
+      </a>
       <a className="hover:text-gray-600" href="https://github.com/livchits">
-        Check the code
+        Check the code here
       </a>
     </footer>
   );
