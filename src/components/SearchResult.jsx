798196012478ded77abc0b8dfebb1b5f548ef6d2
Lucas Livchits <livchitsl@gmail.com>
2021-01-25 21:28:19 -0300

    Fix styles

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -6,7 +6,7 @@ import ImagesFound from './ImagesFound';
 function SearhResult({ error, dataImages }) {
   if (error) {
     return (
-      <div className="w-10/12 max-w-screen-md px-6 py-12 mx-auto mt-12 font-mono text-xl leading-relaxed text-center text-gray-800 bg-red-700 rounded-lg shadow-sm bg-opacity-5">
+      <div className="w-10/12 px-6 py-12 mx-auto mt-12 font-mono text-xl leading-relaxed text-center text-gray-800 bg-red-700 rounded-lg shadow-sm max-w-max bg-opacity-5">
         Sorry, something went wrong{' '}
         <span aria-label="An poop emoji" role="img">
           💩
