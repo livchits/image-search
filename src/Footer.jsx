3de95b91806d01f0c1410940a2b8c7257f894dc2
Lucas Livchits <livchitsl@gmail.com>
2021-01-27 15:01:03 -0300

    Fix footer styles

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,14 +3,17 @@ import * as React from 'react';
 function Footer() {
   return (
     <footer className="w-5/6 max-w-screen-sm pt-2 pb-2 m-6 mx-auto font-mono text-base leading-relaxed text-center text-gray-800 bg-white rounded-md bg-opacity-20 sm:flex sm:justify-center">
-      <a className="sm:mr-4" href="https://lucasdev.me">
+      <a
+        className="block hover:text-gray-500 sm:mr-4"
+        href="https://lucasdev.me"
+      >
         Made by Lucas Livchits{' '}
         <span aria-label="The emoji of a rocket flying" role="img">
           🚀
         </span>
       </a>
       <a
-        className="hover:text-gray-600"
+        className="block hover:text-gray-500"
         href="https://github.com/livchits/image-search"
       >
         Check the code here
