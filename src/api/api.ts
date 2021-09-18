3a918c974ef1c9ee1043ac7c182ad81056d86914
Lucas Livchits <livchitsl@gmail.com>
2021-09-17 23:59:31 -0300

    Fix import

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import type { ImageData, NormalizedImageData } from './../types/index';
-import normalizeData from 'src/utils/normalizeData';
+import normalizeData from '../utils/normalizeData';
 
 const URL = import.meta.env.SNOWPACK_PUBLIC_API_URL;
 const ACCESS_KEY = import.meta.env.SNOWPACK_PUBLIC_API_ACCESS_KEY;
